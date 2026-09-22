5a997a7d6d3309407f3a848931012b41137e922f
sunitha Anilkumar <sunitha181288@gmail.com>
2026-09-22 20:10:22 +0800

    Stage 2: Spring Boot service with health check

---
+++ telemetry-service/src/main/java/com/smartroom/telemetry_service/TelemetryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartroom.telemetry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryServiceApplication.class, args);
+	}
+
+}
